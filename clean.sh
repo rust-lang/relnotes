a07b2919a2ac070a0023b2e9d99a08c345352807
Tyler Mandry <tmandry@google.com>
2022-04-28 18:43:12 -0700

    Make clean.sh work on macOS

--- clean.sh
+++ clean.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    inplace=(-i '')
+else
+    inplace=-i
+fi
+
 rg '\[([^]]*)\]: ' -o release.md > defs
 rg '\[([^]]*)\]$' -o release.md > used
 
@@ -7,6 +13,8 @@ for word in `cat defs`; do
     pattern=$(echo "$word" | sed 's@\[@\\[@g' | sed 's@]@\\]@g' | sed 's@/@\\/@g')
     in_used=$(echo "$word" | sed 's@:.*@@')
     if ! fgrep -q "$in_used" used; then
-        sed -i "/^$pattern/d" release.md
+        sed $inplace -e "/^$pattern/d" release.md
     fi
 done
+
+rm defs used
